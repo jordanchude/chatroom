0890d224056cc79f1bdc8e58928b539bfc5d10df
Jordan <jordan@chude.co>
2021-07-19 09:59:26 -0400

    welcome component

---
+++ src/components/Welcome.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="welcome">
+        <h1>Welcome to Coder Paradise!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Welcome',
+        props: {
+            msg: String
+        }
+    }
+</script>
+    
+<style>
+h1 {
+    color: red;
+}
+</style>
